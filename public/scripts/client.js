9e092f4c0f76b244b77081ab77b3ad28e6d41f40
Valentin Vlad <valentin_gabriel.vlad@yahoo.com>
2021-12-16 03:25:58 -0500

    add rendertweets and remove dummy articles from index.html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,27 +4,32 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const createTweetElement = (tweet, article) => {
-  const time = timeago.format(tweetData.created_at);
-  console.log(time);
+const createTweetElement = (tweet) => {
   const $tweet = $(`<article class="tweet">
   <header>
     <div>
     <div>
-    <img src="${tweetData.user.avatars}"><span>${tweetData.user.name}</span>
+    <img src="${tweet.user.avatars}"><span>${tweet.user.name}</span>
     </div>
-    <div>${tweetData.user.handle}</div></div>
+    <div>${tweet.user.handle}</div></div>
   </header>
-  <p>${tweetData.content.text}</p>
+  <p>${tweet.content.text}</p>
   <footer>
     <div class="arrange-to-top">
-    <span class="timeago" datetime="${timeago.format(tweetData.created_at)}"></span>
+    <span class="timeago" datetime="${timeago.format(tweet.created_at)}"></span>
       <div><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></div>
     </div>
     </footer>
 </article>`);
   return $tweet;
 }
+
+const renderTweets = (tweets) => {
+  for (const tweet of tweets) {
+    $('#tweets-container').append(createTweetElement(tweet));
+  }
+}
+
 
 const tweetData = {
   "user": {
@@ -37,13 +42,37 @@ const tweetData = {
   },
   "created_at": 1461116232227
 }
+
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
 jQuery(document).ready(function() {
 
+  renderTweets(data);
 
-  const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
   timeago.render(document.querySelectorAll('.timeago'));
 });
