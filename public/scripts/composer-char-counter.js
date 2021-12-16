e8f710a94524f1fd2c60e3b32b5e56773930df71
Valentin Vlad <valentin_gabriel.vlad@yahoo.com>
2021-12-15 20:20:32 -0500

    counter function works

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
   // --- our code goes here ---
+  const tweetSize = 140;
+  console.log("let's count!");
+  const txtArea = $("#tweet-text");
+  let count = $("output.counter");
+  txtArea.on('keyup', (event) => {
+
+    const currCounter = tweetSize - event.target.value.length;
+    if (currCounter < 0) {
+      count.attr('style', 'color: red;');
+    }
+    count.text(currCounter);
+
+  });
+
 });
