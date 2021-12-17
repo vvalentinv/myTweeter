0c466686ea8423e864470316f33befa02915012f
Valentin Vlad <valentin_gabriel.vlad@yahoo.com>
2021-12-16 23:57:55 -0500

    fixed counter color to revertto non red font color when reverting to an allowed character limit, fixed the slide in and out error messages. Functional project

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 const URL = 'http://localhost:8080/tweets';
+const tweetSize = 140;
 
 const createTweetElement = (tweet) => {
 
@@ -11,18 +12,26 @@ const createTweetElement = (tweet) => {
   <header>
     <div>
     <div>
-    <img src="${tweet.user.avatars}"><span>${tweet.user.name}</span>
+    <img id="avatar" src=""><span id="username"></span>
     </div>
-    <div>${tweet.user.handle}</div></div>
+    <div id="handle"></div></div>
   </header>
-  <p>${tweet.content.text}</p>
+  <p id="text"></p>
   <footer>
     <div class="arrange-to-top">
-    <span class="timeago">${timeago.format(tweet.created_at)}</span>
+    <span class="timeago"></span>
       <div><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></div>
     </div>
     </footer>
 </article>`);
+
+  //build the article and ensure the loading of sanitized data
+  $tweet.find("#avatar").attr({ src: tweet.user.avatars });
+  $tweet.find("#username").text(tweet.user.name);
+  $tweet.find("#handle").text(tweet.user.handle);
+  $tweet.find("#text").text(tweet.content.text);
+  $tweet.find(".timeago").text(timeago.format(tweet.created_at));
+
   return $tweet;
 }
 
@@ -61,29 +70,36 @@ const getTweets = (method, url) => {
 
 
 jQuery(document).ready(function() {
+  //populate the divs with their respective error messages to avoid the delay caused by the error trigger
+  $("#error1").text("The tweet must contain at least one character");
+  $("#error2").text("The tweet cannot contain more than 140 characters");
+
+  //focus on text area and render tweets
   $('#tweet-text').focus();
-  getTweets('GET', URL);//load tweets onpage load;
+  getTweets('GET', URL);
 
   // event listener for for form submit
   $('.new-tweet form').on('submit', function(event) {
+
     // prevent the default behavior of the form submission
     event.preventDefault();
 
     const tweetSize = $('#tweet-text').val();
     //prevent the post of an invalid tweet
-    if (tweetSize.length < 1 || tweetSize.length > 140) {
-      $('#tweet-text').val('');
-      return alert("the tweet must contain at least one character and no more than 140");
+    //return alert("the tweet must contain at least one character and no more than 140");
+    if (tweetSize.length < 1) {
+      $("#error1").slideDown(700);
+    } else if (tweetSize.length > 140) {
+      $("#error2").slideDown(700);
+    } else {
+      const data = $(this).serialize();
 
+      $.post(URL, data, () => {
+        //retrieve twets after post of new tweet completed and clear the textarea and its counter
+        getTweets('GET', URL);
+        $('#tweet-text').val('');
+        $('#counter1').text('140');
+      });
     }
-
-    // const tweetSize = $('#tweet-text').val();
-
-    const data = $(this).serialize();
-
-    $.post(URL, data, () => {
-      getTweets('GET', URL);//retrieve twets after post of new tweet completed
-    });
   });
-
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,10 +3,20 @@ $(document).ready(function() {
   const tweetSize = 140;
   let count = $("output.counter");
 
-  $("#tweet-text").on('keyup', (event) => {
+  $("#tweet-text").on('input', (event) => {
     const currCounter = tweetSize - event.target.value.length;
     if (currCounter < 0) {
-      count.attr('style', 'color: red;');
+      $("#error2").slideDown(700);
+      $("#error1").slideUp(700);
+      count.addClass('redCounter');
+    } else if (currCounter === 140) {
+      $("#error1").slideDown(700);
+      $("#error2").slideUp(700);
+
+    } else {
+      $("#error2").slideUp(700);
+      $("#error1").slideUp(700);
+      count.removeClass('redCounter');
     }
     count.text(currCounter);
   });
